f47ec2b0a1b35a0dfea8f08caa0d7c62f0e30b5a
whiteSkar <sypower07@hotmail.com>
2015-02-13 23:17:37 -0800

    Not robust domain splitting is done. Finds solution for Easy and Hard but not the rest..

--- cpsc322-sudoku/src/sudoku/SudokuSolver.java
+++ cpsc322-sudoku/src/sudoku/SudokuSolver.java
@@ -2,6 +2,7 @@ package sudoku;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -128,7 +129,7 @@ public class SudokuSolver
 			if (arcMap.containsKey(thisNode))
 			{
 				ArrayList<Pair<Integer, Integer>> otherNodes = arcMap.get(thisNode);
-				otherNodes.add(otherNode);	// check if this is actually added ot the arcMap
+				otherNodes.add(otherNode);
 			}
 			else
 			{
@@ -138,11 +139,45 @@ public class SudokuSolver
 			}
 		}
 		
+		List<Integer> isFound = new ArrayList<Integer>();	// Need some object that is mutable to check if solution is found
+		List<List<List<Integer>>> resultDomains = new ArrayList<List<List<Integer>>>();
+		for (int i = 0; i < 9; i++)
+		{
+			resultDomains.add(new ArrayList<List<Integer>>());
+			for (int j = 0; j < 9; j++)
+			{
+				resultDomains.get(i).add(new ArrayList<Integer>());
+			}
+		}
+		solveHelper(arcMap, toDoArcs, domains, resultDomains, isFound);
+		
+		for (int i = 0; i < resultDomains.size(); i++)
+		{
+			for (int j = 0; j < resultDomains.get(i).size(); j++)
+			{
+				List<Integer> domainsTemp = resultDomains.get(i).get(j);
+				for (int k = 0; k < domainsTemp.size(); k++)
+				{
+					if (domainsTemp.size() == 1)
+						board[i][j] = domainsTemp.get(k);
+				}
+			}
+		}
+		
+		return board;
+	}
+	
+	private void solveHelper(Map<Pair<Integer, Integer>, ArrayList<Pair<Integer, Integer>>> arcMap, 
+							Set<Pair<Pair<Integer, Integer>, Pair<Integer, Integer>>> toDoArcs, 
+							List<List<List<Integer>>> domains,
+							List<List<List<Integer>>> resultDomains,
+							List<Integer> isFound)
+	{		
 		while (!toDoArcs.isEmpty())
 		{
 			@SuppressWarnings("unchecked")
 			Pair<Pair<Integer, Integer>, Pair<Integer, Integer>> arc = 
-					(Pair<Pair<Integer, Integer>, Pair<Integer, Integer>>) toDoArcs.toArray()[0];
+					(Pair<Pair<Integer, Integer>, Pair<Integer, Integer>>) toDoArcs.iterator().next();
 			toDoArcs.remove(arc);
 			
 			Pair<Integer, Integer> thisNode = arc.first;
@@ -164,46 +199,104 @@ public class SudokuSolver
 			
 			if (countOfDomainOfThisNodeAfterPruning != countOfDomainOfThisNodeBeforePruning)
 			{				
-				ArrayList<Pair<Integer, Integer>> otherNodesThatAreConnectedToTheConstraintsThatNeedToBeReconsidered = 
-						arcMap.get(thisNode);
+				addArcsToReconsider(arcMap, toDoArcs, thisNode);
+			}
+		}
+		
+		for (int i = 0; i < domains.size(); i++)
+		{
+			for (int j = 0; j < domains.get(i).size(); j++)
+			{
+				int numberOfDomainValues = domains.get(i).get(j).size();
+				//System.out.println("Solver i: " + i + "    j: " + j + "     Number of values: " + numberOfDomainValues);
+				if (numberOfDomainValues == 0)
+					return;
 				
-				for (int i = 0; i < otherNodesThatAreConnectedToTheConstraintsThatNeedToBeReconsidered.size(); i++)
+				//printDomains(domains);
+				if (numberOfDomainValues > 1)
 				{
-					Pair<Integer, Integer> otherNodeToReconsider = otherNodesThatAreConnectedToTheConstraintsThatNeedToBeReconsidered.get(i);
-					Pair<Pair<Integer, Integer>, Pair<Integer, Integer>> arcToReconsider = 
-							new Pair<Pair<Integer, Integer>, Pair<Integer, Integer>>(otherNodeToReconsider, thisNode);
-					
-					if (!toDoArcs.contains(arcToReconsider))
+					//System.out.println("Splitting Domain");
+					int midIndex = (int) Math.ceil((double)numberOfDomainValues / 2);
+					List<List<List<Integer>>> copyOfDomains = getCopyOfDomains(domains);
+					List<Integer> valuesOfDomain = copyOfDomains.get(i).get(j);
+					List<Integer> copyOfValueOfDomain = new ArrayList<Integer>(valuesOfDomain);
+
+					List<Integer> splitDomain1 = copyOfValueOfDomain.subList(0, midIndex);
+					valuesOfDomain.clear();
+					valuesOfDomain.addAll(splitDomain1);
+					addArcsToReconsider(arcMap, toDoArcs, new Pair<Integer, Integer>(i, j));
+					solveHelper(arcMap, toDoArcs, copyOfDomains, resultDomains, isFound);
+					if (!isFound.isEmpty())
 					{
-						toDoArcs.add(arcToReconsider);
+						return;
+					}
+
+					copyOfDomains = getCopyOfDomains(domains);
+					valuesOfDomain = copyOfDomains.get(i).get(j);
+					List<Integer> splitDomain2 = copyOfValueOfDomain.subList(midIndex, numberOfDomainValues);
+					valuesOfDomain.clear();
+					valuesOfDomain.addAll(splitDomain2);
+					addArcsToReconsider(arcMap, toDoArcs, new Pair<Integer, Integer>(i, j));	// area of performance improvement
+					solveHelper(arcMap, toDoArcs, copyOfDomains, resultDomains, isFound);
+					if (!isFound.isEmpty())
+					{
+						return;
 					}
 				}
 			}
 		}
 		
-		System.out.println();
-		for (int i = 0; i < 9; i++)
+		isFound.add(new Integer(0));
+		if (!isFound.isEmpty()) 
 		{
-			for (int j = 0; j < 9; j++)
+			for (int i = 0; i < domains.size(); i++)
 			{
-				List<Integer> domainsTemp = domains.get(i).get(j);
-				for (int k = 0; k < domainsTemp.size(); k++)
+				for (int j = 0; j < domains.get(i).size(); j++)
 				{
-					System.out.print(domainsTemp.get(k));
-					
-					if (domainsTemp.size() == 1)
-						board[i][j] = domainsTemp.get(k);
+					for (int k = 0; k < domains.get(i).get(j).size(); k++)
+					{
+						resultDomains.get(i).get(j).addAll(domains.get(i).get(j));
+					}
 				}
-				
-				System.out.print(" ");
 			}
-			System.out.println();
+			System.out.println("Found Solution");
+			printDomains(resultDomains);
 		}
-		
-		return board;
 	}
 	
-	public boolean doesOtherNodeContainAnyIntOtherThanThisInt(int thisInt, List<Integer> otherNodeDomain)
+	private void addArcsToReconsider(Map<Pair<Integer, Integer>, ArrayList<Pair<Integer, Integer>>> arcMap, 
+									 Set<Pair<Pair<Integer, Integer>, Pair<Integer, Integer>>> toDoArcs,
+									 Pair<Integer, Integer> nodeInWhichItsDomainIsSplitted)
+	{
+		ArrayList<Pair<Integer, Integer>> otherNodesThatAreConnectedToTheConstraintsThatNeedToBeReconsidered = 
+				arcMap.get(nodeInWhichItsDomainIsSplitted);
+		
+		for (int i = 0; i < otherNodesThatAreConnectedToTheConstraintsThatNeedToBeReconsidered.size(); i++)
+		{
+			Pair<Integer, Integer> otherNodeToReconsider = otherNodesThatAreConnectedToTheConstraintsThatNeedToBeReconsidered.get(i);
+			Pair<Pair<Integer, Integer>, Pair<Integer, Integer>> arcToReconsider = 
+					new Pair<Pair<Integer, Integer>, Pair<Integer, Integer>>(otherNodeToReconsider, nodeInWhichItsDomainIsSplitted);
+			
+			toDoArcs.add(arcToReconsider);
+		}
+	}
+	
+	private List<List<List<Integer>>> getCopyOfDomains(List<List<List<Integer>>> domains)
+	{
+		List<List<List<Integer>>> copyOfDomain = new ArrayList<List<List<Integer>>>();
+		for (int i = 0; i < domains.size(); i++)
+		{
+			copyOfDomain.add(new ArrayList<List<Integer>>());
+			for (int j = 0; j < domains.get(i).size(); ++j)
+			{
+				copyOfDomain.get(i).add(new ArrayList<Integer>(domains.get(i).get(j)));
+			}
+		}
+		
+		return copyOfDomain;
+	}
+	
+	private boolean doesOtherNodeContainAnyIntOtherThanThisInt(int thisInt, List<Integer> otherNodeDomain)
 	{
 		for (int i = 0; i < otherNodeDomain.size(); ++i)
 		{
@@ -213,6 +306,25 @@ public class SudokuSolver
 		}
 		
 		return false;
+	}
+	
+	private void printDomains(List<List<List<Integer>>> domains)
+	{
+		System.out.println();
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+			{
+				List<Integer> domainsTemp = domains.get(i).get(j);
+				for (int k = 0; k < domainsTemp.size(); k++)
+				{
+					System.out.print(domainsTemp.get(k));
+				}
+				
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
 	}
 	
 	public class Pair<A, B>
